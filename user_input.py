198538613f71a45f52c6ff2f2792233c04fed296
Godwinchiomafaith <chiomagodwinf@gmail.com>
2024-11-10 16:51:07 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ") 
+
+age = input("How old are you? ")
+
+location = input("where is your location? ")
+
+print (f"hello chioma, you are 25 year old and leave in edo state nigeria.")
+
+
